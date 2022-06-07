dd6f4036dec1fa7324302ea10caa387b1f4ae013
seokhyun <gah1234@naver.com>
2022-06-07 14:11:37 +0900

    로그인 백업

--- user/forms.py
+++ user/forms.py
@@ -3,30 +3,34 @@ from django.contrib.auth.hashers import check_password
 
 from user.models import User
 
-class RegisterForm(forms.Form): # forms.Form을 상속받아요.
+from django import forms
+from .models import User
+from django.contrib.auth.hashers import check_password
+
+
+class RegisterForm(forms.Form):
     email = forms.EmailField(
-        error_messages={    # 입력하지 않을시 생성되는 오류 메시지
-        'required': '이메일을 입력해주세요'
-        },
-        max_length=64,
-        label='이메일',
+        error_messages={'required': "이메일을 입력하세요."},
+        max_length=64, label="이메일"
     )
     password = forms.CharField(
-        error_messages={
-            'required':'비밀번호를 입력하세요'
-        },
-        widget=forms.PasswordInput, # 추측컨데 비밀번호 입력 요건을 갖춘 입력방식이 될듯함.
-        label='비밀번호'            # 라벨 이름을 표기함.
+        error_messages={'required': "비밀번호를 입력하세요"},
+        widget=forms.PasswordInput, label="비밀번호"
     )
     re_password = forms.CharField(
-        error_messages={
-            'required':'비밀번호를 입력하세요'
-        },
-        widget=forms.PasswordInput,
-        label='비밀번호'
+        error_messages={'required': "비밀번호를 입력하세요"},
+        widget=forms.PasswordInput, label="비밀번호 재입력"
     )
-    model = User
-    fields = [email, password]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+        re_password = cleaned_data.get('re_password')
+
+        if password and re_password:
+            if password != re_password:
+                self.add_error('password', '비밀번호가 서로 다릅니다.')
 
 
 
--- user/views.py
+++ user/views.py
@@ -1,34 +1,45 @@
+from django.contrib.auth.hashers import make_password
 from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView  # form을 활용할수 있게 해주는 클래스에요
 
 # Create your views here.
 from user.forms import RegisterForm, LoginForm
+from user.models import User
 
+
+def logout(request):
+    if request.session.get('user'):
+        del (request.session['user'])
+    return redirect('/')
 
 def index(request):
     return render(request, 'index.html')
 
+def home(request):
+    return render(request, 'home.html', {'user' : request.session.get('user')})
+
 
 class RegisterView(FormView):
     template_name = 'register.html'
+    form_class = RegisterForm
     success_url = '/'
-    # template_name이라고 하면 html파일이조? 이게 부모인 FormView에서 가져온거에요.
 
-    form_class = RegisterForm  # 위의 임포트든 된것을 form_class의 값으로 할당해줌.
+    def form_valid(self, form):
+        user = User(
+            email=form.data.get('email'),
+            password=make_password(form.data.get('password')),
+        )
+        user.save()
+
+        return super().form_valid(form)
 
 
 class LoginView(FormView):
     template_name = 'login.html'  # template_name이라고 하면 html파일이조? 이게 부모인 FormView에서 가져온거에요.
     form_class = LoginForm
-    success_url = ''
+    success_url = '/'
 
     # LoginView.as_view()가 실행될 때
     def form_valid(self, form):  # form유효성(데이터가 정상일때)을 마쳤을 때 사용해요. 로그인이 정상적으로 되었는 경우!
-        self.request.session['user'] = form.email  # sessuib에 키값 user를 생성하는데요. 그 값은 form의 email속성에서 가져다와요.
-
+        self.request.session['user'] = form.data.get('email')  # sessuib에 키값 user를 생성하는데요. 그 값은 form의 email속성에서 가져다와요.
         return super().form_valid(form)  # 부모클래스 FormView에 있는 form_valid()메소드를 호출합니다.
-
-    def logout(request):
-        if request.session.get('user'):
-            del (request.session['user'])
-        return redirect('/')
